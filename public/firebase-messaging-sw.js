6d4c996655516e09a25e0cb551774799632d6a4b
Kwabena Asumadu <codinnovations@gmail.com>
2024-07-10 00:50:04 -0700

    changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,23 +1,22 @@
 // public/firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging.js');
 
 firebase.initializeApp({
-   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDING_ID,
-   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+   apiKey: "AIzaSyB79Ue_0CkpaBTISGIGWtYSAvPJ6cSwbtI",
+   authDomain: "cod-shop-c1874.firebaseapp.com",
+   databaseURL: "https://cod-shop-c1874-default-rtdb.firebaseio.com",
+   projectId: "cod-shop-c1874",
+   storageBucket: "cod-shop-c1874.appspot.com",
+   messagingSenderId: "24922895227",
+   appId: "1:24922895227:web:1d2dc54aa38da4dff903de",
+   measurementId: "G-XTHD2EN5TZ"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
   console.log('Received background message ', payload);
-  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
