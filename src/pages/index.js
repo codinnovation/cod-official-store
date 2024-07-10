c193cef8b1f8ae8001bae7459ffa04962463c917
Kwabena Asumadu <codinnovations@gmail.com>
2024-07-10 03:21:24 -0700

    implemented login and logout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
+import withSession from "./api/session";
+import HomePage from "../pages/home";
 
-export default function Home() {
+export default function Home({ user }) {
   return (
     <>
       <Head>
@@ -11,7 +13,30 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div></div>
+      <div>
+        <HomePage user={user} />
+      </div>
     </>
   );
 }
+
+export const getServerSideProps = withSession(async function ({ req, res }) {
+  const user = req.session.get("user");
+  if (!user) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  if (user) {
+    req.session.set("user", user);
+    await req.session.save();
+  }
+  return {
+    props: {
+      user: user,
+    },
+  };
+});
