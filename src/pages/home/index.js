fc8429c779c93025ca2cf65066999d7e7f7a54df
Kwabena Asumadu <codinnovations@gmail.com>
2024-07-10 00:34:01 -0700

    added notifications

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -14,6 +14,7 @@ import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
 import MoreIcon from "@mui/icons-material/More";
 import { LogoutRounded } from "@mui/icons-material";
 
+
 function Index() {
   return (
     <>
